4636ecbacbc5b7e0dbf5282532957c6b8ba8edaf
Arun chaitanya <>
2019-05-20 12:53:54 +1000

    Adding documentation and refactoring code

--- project/articles_sentiment_analysis.py
+++ project/articles_sentiment_analysis.py
@@ -5,25 +5,28 @@ import requests
 from requests.compat import urljoin
 import urllib
 
-
+# Cleaning the string
 def clean_article(article):
     return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", article).split())
 
-
+# Perform
 def get_article_sentiment(article):
-    # create TextBlob object of passed tweet text
+    # perform text analysis using `TextBlob`.
     analysis = TextBlob(clean_article(article))
     print(analysis.sentiment)
+    # Check polarity
     if analysis.sentiment.polarity > 0:
         sentiment = 'positive'
     elif analysis.sentiment.polarity == 0:
         sentiment = 'neutral'
     else:
         sentiment = 'negative'
-
+    # returns an array of text sentiment, polarity and subjectivity.
     return [sentiment, analysis.sentiment.polarity, analysis.sentiment.subjectivity]
 
 
+# Splits the multiple entity names and sets the symbols
+# After getting the symbols join them with ',' and return a string.
 def get_article_entity_symbols(ents):
     article_entites = ents.split(',')
     symbols = []
@@ -39,12 +42,17 @@ def get_article_entity_symbols(ents):
             return symbols[0]
 
 
+# Gets symbol of entity listed in stock markets around the world using yahoo finance.
 def get_stock_name_for_company(entity):
+    # base url
     base = 'https://query2.finance.yahoo.com/v1/finance/search'
+    # some static url config like number of search results to be returned `quotesCount`
     other_query = "&quotesCount=3&newsCount=0&quotesQueryId=tss_match_phrase_query&" \
                    "multiQuoteQueryId=multi_quote_single_token_query&newsQueryId=news_ss_symbols" \
                    "&enableCb=false&enableNavLinks=false"
+    # convert entity name to url encoded format
     encode_entity = urllib.parse.quote(entity)
+    # config final url query for getting symbol of entity
     url_query = urljoin(base, '?q='+encode_entity+other_query)
     print(url_query)
     request = requests.get(url_query)
@@ -57,12 +65,13 @@ def get_stock_name_for_company(entity):
                     quote_type = quote['quoteType']
                     if quote_type == 'EQUITY':
                         return quote['symbol']
+                        # Exit the loop when first result of quote_type is 'EQUITY'
                         exit()
         else:
             print('no data returned')
 
 
-# get articles from `.csv` and returns an `order_dictonary_list`
+# get articles from `.csv` and returns an `order dictionary list`.
 def get_articles():
     with open('sample_ent.csv', newline='') as csvfile:
         articles_data = csv.DictReader(csvfile)
@@ -72,6 +81,7 @@ def get_articles():
     return articles_list
 
 
+# Peform sentiment analysis only when entity symbol identified.
 def perform_analysis():
     # Get articles list
     articles = get_articles()
